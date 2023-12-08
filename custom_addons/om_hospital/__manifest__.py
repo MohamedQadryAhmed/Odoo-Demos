7a3a4f50d104176ef6cd821700f1e304948cfc05
mohamedqadryahmed <mohamedqadryahmed@gmail.com>
2023-12-08 13:42:38 +0200

    create custom module

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'sequence': -500,
+    'application': True,
+    'data':[
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+}
